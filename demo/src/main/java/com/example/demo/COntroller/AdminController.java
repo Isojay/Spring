64429a7fb9b8e838c74644658c58bd08d2f57903
Isojay <isongum@gmail.com>
2023-05-03 16:47:18 +0545

    admin

--- demo/src/main/java/com/example/demo/COntroller/AdminController.java
+++ demo/src/main/java/com/example/demo/COntroller/AdminController.java
@@ -1,23 +1,94 @@
 package com.example.demo.COntroller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.logging.log4j.message.ReusableMessage;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.Repo.CategoryService;
+import com.example.demo.dao.Category;
 
 @Controller
 
 public class AdminController {
 	
+	CategoryService categoryService;
+	
+	
+	public AdminController(CategoryService thecategoryService) {
+		super();
+		categoryService = thecategoryService;
+	}
+	
 	@GetMapping("/admin")
 	public String showHome() {
 		return "adminHome";
 	}
+	@GetMapping("admin/categories")
+	public String showCategories(Model theModel) {
+		
+		List<Category> theCategories = categoryService.findAll();
+		theModel.addAttribute("categories", theCategories);
+		
+		return "categories";
+	}
 	
+	@GetMapping("/admin/categories/add")
+	public String addCategories(Model theModel) {
+		
+		Category categories = new Category();
+		
+		theModel.addAttribute("category",categories);
+		
+		return "categoriesAdd";
+	}
 	
-	
-	
-	@GetMapping("/login")
-	public String showLogin() {
-		return "login";
+	@PostMapping("/admin/categories/add")
+	public String saveCategories(@ModelAttribute("category") Category category ) {
+		
+		categoryService.save(category);
+		
+		return "redirect:/admin/categories";
+	}
+	/*
+	@GetMapping("/admin/categories/delete/{id}")
+	public String delcategories(@PathVariable int id) {
+		
+		categoryService.deleteById(id);
+		
+		return "redirect:/admin/categories";
+	}*/
+	@GetMapping("/admin/categories/delete")
+	public String delcategories(@RequestParam("id") int id) {
+		
+		categoryService.deleteById(id);
+		
+		return "redirect:/admin/categories";
+	}
+	@GetMapping("/admin/categories/update/{id}")
+	public String updatecategories(@PathVariable int id,Model theModel) {
+		
+		Optional<Category> category = categoryService.findById(id);
+		if(category.isPresent()) {
+			
+			theModel.addAttribute("category",category.get());
+			return "categoriesAdd";
+			
+		}else {
+			return "404";
+		}
+		
+		
+		
+		
+		
 	}
 	
 }
